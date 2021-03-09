32e74df7c4c3684568a6b6beee572511a3c4e8fa
Amhasilassie <80092888+Amhasilassie@users.noreply.github.com>
2021-03-09 12:19:25 +0300

    Add files via upload

---
+++ behavior.js
@@ -0,0 +1,89 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+	var thumbnailElemant = document.getElementById("smart_thumbnail");
+	thumbnailElemant.addEventListener("click", function(){
+		if (thumbnailElemant.className == ""){
+			thumbnailElemant.className = "small";
+		}
+		else {
+			thumbnailElemant.className = "";
+		}
+	});
+});
+document.addEventListener("DOMContentLoaded", function(event) {
+	var thumbnailElemant = document.getElementById("senkelle");
+	thumbnailElemant.addEventListener("click", function(){
+		if (thumbnailElemant.className == ""){
+			thumbnailElemant.className = "small";
+		}
+		else {
+			thumbnailElemant.className = "";
+		}
+	});
+});
+document.addEventListener("DOMContentLoaded", function(event) {
+	var thumbnailElemant = document.getElementById("cheetah");
+	thumbnailElemant.addEventListener("click", function(){
+		if (thumbnailElemant.className == ""){
+			thumbnailElemant.className = "small";
+		}
+		else {
+			thumbnailElemant.className = "";
+		}
+	});
+});
+document.addEventListener("DOMContentLoaded", function(event) {
+	var thumbnailElemant = document.getElementById("omo");
+	thumbnailElemant.addEventListener("click", function(){
+		if (thumbnailElemant.className == ""){
+			thumbnailElemant.className = "small";
+		}
+		else {
+			thumbnailElemant.className = "";
+		}
+	});
+});
+document.addEventListener("DOMContentLoaded", function(event) {
+	var thumbnailElemant = document.getElementById("Nile_crocodile");
+	thumbnailElemant.addEventListener("click", function(){
+		if (thumbnailElemant.className == ""){
+			thumbnailElemant.className = "small";
+		}
+		else {
+			thumbnailElemant.className = "";
+		}
+	});
+});
+document.addEventListener("DOMContentLoaded", function(event) {
+	var thumbnailElemant = document.getElementById("Walia_ibex");
+	thumbnailElemant.addEventListener("click", function(){
+		if (thumbnailElemant.className == ""){
+			thumbnailElemant.className = "small";
+		}
+		else {
+			thumbnailElemant.className = "";
+		}
+	});
+});
+document.addEventListener("DOMContentLoaded", function(event) {
+	var thumbnailElemant = document.getElementById("leopard");
+	thumbnailElemant.addEventListener("click", function(){
+		if (thumbnailElemant.className == ""){
+			thumbnailElemant.className = "small";
+		}
+		else {
+			thumbnailElemant.className = "";
+		}
+	});
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	var thumbnailElemant = document.getElementById("owel");
+	thumbnailElemant.addEventListener("click", function(){
+		if (thumbnailElemant.className == ""){
+			thumbnailElemant.className = "small";
+		}
+		else {
+			thumbnailElemant.className = "";
+		}
+	});
+});
